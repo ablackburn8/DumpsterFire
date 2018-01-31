6cb83237e827ae5d7e8a66dd69e563e2ffd91b95
ablackburn8 <35774769+ablackburn8@users.noreply.github.com>
2018-01-31 11:07:14 -0600

    added rubric 2

---
+++ Rubric2/beetroot.js
@@ -0,0 +1,8 @@
+	function subtractNumbers(a, b){
+		"use strict";
+		var endResult= a -b;
+		return endResult;
+	}
+	
+var subtractNumbers2 = subtractNumbers(34, 17);		
+alert (subtractNumbers2);// JavaScript Document
