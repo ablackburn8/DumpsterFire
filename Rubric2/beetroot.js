8c0a79e15f697e49e17474d9ef2efeb136745634
ablackburn8 <35774769+ablackburn8@users.noreply.github.com>
2018-01-31 11:39:25 -0600

    added Switch statement

--- Rubric2/beetroot.js
+++ Rubric2/beetroot.js
@@ -1,4 +1,19 @@
-	function subtractNumbers(a, b){
+var ultimateQuestion= prompt ("Pineapple on pizza?");
+
+switch(ultimateQuestion) {
+		case "Yes":
+		alert("Cool.");
+		break;
+		
+		case "No":
+		alert("Some people don't like it.");
+		break;
+		
+	default:
+		alert("I feel like you're dodging the question.");
+}
+
+		function subtractNumbers(a, b){
 		"use strict";
 		var endResult= a -b;
 		return endResult;
