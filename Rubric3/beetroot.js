a55d15d55d5feaec0f9ea67182900f04a57e9db8
ablackburn8 <35774769+ablackburn8@users.noreply.github.com>
2018-02-07 11:45:08 -0600

    Rubric 3
    Rubric 3

---
+++ Rubric3/beetroot.js
@@ -0,0 +1,22 @@
+var a= 1;
+
+while (a<8){
+	alert (a);
+	a++;
+}
+
+var coolNames = new Array();
+coolNames[0]="asher";
+coolNames[1]="orchid";
+coolNames[2]="mad";
+coolNames[3]="tuesday";
+
+coolNames.sort();
+document.write(coolNames);
+
+
+var pencilType={material:"wood", lead: "HB", kind: "graphite"};
+
+document.write(pencilType.lead);
+
+// JavaScript Document
