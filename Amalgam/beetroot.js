56accc9ae405e1544a863a7a759ca90ed3d6508b
ablackburn8 <35774769+ablackburn8@users.noreply.github.com>
2018-03-07 20:42:13 -0600

    Client Side Update
    condensed code

--- Amalgam/beetroot.js
+++ Amalgam/beetroot.js
@@ -6,7 +6,7 @@ var comics = document.getElementById("comics");
 var comicstext = document.getElementById("comicstext");
 console.log(films);
 
-films.addEventListener("click", function(){
+/*films.addEventListener("click", function(){
 	if(filmstext.className === "hide"){
 		filmstext.className = "";
 	}else {filmstext.className = "hide";}
@@ -21,10 +21,29 @@ comics.addEventListener("click", function(){
 	if(comicstext.className === "hide"){
 		comicstext.className = "";
 	}else {comicstext.className = "hide";}
-});	
-						
-						
-						
-						
+});*/
+
+films.addEventListener("click", linkThing);
+books.addEventListener("click", linkThing);
+comics.addEventListener("click", linkThing);
+
+function linkThing(){
+	var linkId = this.attributes["data-id"].value;
+	var link = document.getElementById(linkId);
+		if(link.className === "hide"){
+		link.className = "";
+	}else {link.className = "hide";}
+}
+			
+	
+
+
+
+
+
+
+
+
+
 						
 						
